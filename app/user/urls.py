7b73808e160a699fe75c3480d4fdf0c9a2fe4e93
Girish Madhavan Nambiar <girish.nambiar@hurix.com>
2021-11-06 22:36:12 +0530

    Added endpoint for creating user.

---
+++ app/user/urls.py
@@ -0,0 +1,14 @@
+# from django.contrib import admin
+from django.urls import path
+
+# import our views
+from user import views
+
+# define our app name
+app_name = 'user'
+
+# create our url paths
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+]
